3949d3c16b0ec03543a5bef537c98a138c9a144c
unknown <christianlorza@googlemail.com>
2018-01-11 20:16:43 +0000

    adding all files to schedule upload to droplet

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var port = 3000;
+var mongoose = require("mongoose");
+var jwt = require("jsonwebtoken");
+var uid = require("crypto-random-string");
+var app = express();
+
+var log = function(x) {console.log(x);}
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use("/resources/css", express.static("public/css"));
+app.use("/resources/bulma", express.static("node_modules/bulma"));
+
+app.get("/", function (req, res) {
+    res.render("index")
+})
+
+app.listen(port, () => {
+    log("Server is running B..." + port);
+});
