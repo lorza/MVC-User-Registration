ead0c7d966e4849497b70d0506182285ee3c7d96
unknown <christianlorza@googlemail.com>
2018-01-12 16:48:35 +0000

    models + login linebreak

--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ var userSchema = mongoose.Schema({
 
 userSchema.methods.generateHash = function(password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
+};  
 
 userSchema.methods.validPassword = function (password) {
     return bcrypt.compareSync(password, this.local.password);
